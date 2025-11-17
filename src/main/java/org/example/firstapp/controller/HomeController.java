6b3b8bf539f0c7978a5680c397bf096e1b9875c3
gwangryeol-edu <rhkr0427rhkr@gmail.com>
2025-11-17 15:05:31 +0900

    request param

--- src/main/java/org/example/firstapp/controller/HomeController.java
+++ src/main/java/org/example/firstapp/controller/HomeController.java
@@ -4,9 +4,10 @@ import org.springframework.boot.autoconfigure.amqp.RabbitConnectionDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Controller
 public class HomeController {
@@ -54,5 +55,73 @@ public class HomeController {
         int score = 90;
         model.addAttribute("score", score);
         return "grade";
+    }
+
+    @GetMapping("/lunch")
+    public String lunch(Model model) {
+        List<String> menus = Arrays.asList("라면", "김밥", "돈까스");
+
+        Random random = new Random();
+        String pick = menus.get(random.nextInt(menus.size()));
+
+        model.addAttribute("pick", pick);
+        return "lunch";
+    }
+
+
+//    @GetMapping("/lotto")
+//    public String lotto(Model model) {
+//        List<Integer> numbers = IntStream.rangeClosed(1,45).
+//    }
+
+    @GetMapping("/profile/{username}")
+    public String profile(@PathVariable String username, Model model) {
+        model.addAttribute("username", username);
+        return "profile";
+    }
+
+    @GetMapping("/cube/{number}")
+    public String cube(@PathVariable int number, Model model) {
+        int result = number * number * number;
+        model.addAttribute("number", number);
+        model.addAttribute("result", result);
+        return "cube";
+    }
+
+    // 짝수 홀수 판뱔
+    // number/{num} 짝수인지 홀수인지 판별해서 화면에 출력
+    @GetMapping("/number/{num}")
+    public String number(@PathVariable int num, Model model) {
+        model.addAttribute("num", num);
+        return "number";
+    }
+
+    // 나이 계산 (Year 클래스)
+    // /age/{birthYear} 현재 나이를 계산해서 출력
+    // /age/1990 36살입니다
+    @GetMapping("/age/{birthYear}")
+    public String age(@PathVariable int birthYear, Model model) {
+        Calendar cal = Calendar.getInstance();
+
+        int today = cal.get(Calendar.YEAR);
+        model.addAttribute("birthYear", birthYear);
+        model.addAttribute("today", today);
+        return "age";
+    }
+
+    @GetMapping("/ping")
+    public String ping(Model model) {
+        return "ping";
+    }
+
+    @GetMapping("/pong")
+    public String pong(
+            @RequestParam String title,
+            @RequestParam String content,
+            Model model) {
+
+        model.addAttribute("title", title);
+        model.addAttribute("content", content);
+        return "pong";
     }
 }
