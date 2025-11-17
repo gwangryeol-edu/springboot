1b3af12cd0b408131fd6c04a5c06f23b9880a92a
gwangryeol-edu <rhkr0427rhkr@gmail.com>
2025-11-17 11:49:46 +0900

    mvc model begin

--- src/main/java/org/example/firstapp/controller/HomeController.java
+++ src/main/java/org/example/firstapp/controller/HomeController.java
@@ -1,4 +1,58 @@
 package org.example.firstapp.controller;
 
+import org.springframework.boot.autoconfigure.amqp.RabbitConnectionDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
 public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";  // home.html 로 자동인식, 이동
+    }
+
+    @GetMapping("/hello")
+    // model: 데이터, 데이터를 담아서 view로 전달
+    public String hello(Model model) {
+        String name = "gwangryeol";
+
+        model.addAttribute("name", name);
+
+        return "hello";
+    }
+
+    @GetMapping("/user")
+    public String user(Model model) {
+        model.addAttribute("username", "KIM");
+        model.addAttribute("age", 18);
+        model.addAttribute("city", "seoul");
+
+        return "user";
+    }
+
+    @GetMapping("/fruits")
+    public String fruits(Model model) {
+        List<String> fruitsList = new ArrayList<>();
+        fruitsList.add("apple");
+        fruitsList.add("banana");
+        fruitsList.add("cherry");
+        fruitsList.add("lemon");
+        fruitsList.add("kiwi");
+
+
+        model.addAttribute("fruits", fruitsList);
+        return "fruits";
+    }
+
+    @GetMapping("/grade")
+    public String grade(Model model) {
+        int score = 90;
+        model.addAttribute("score", score);
+        return "grade";
+    }
 }
